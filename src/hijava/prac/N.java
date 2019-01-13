8237b93d202dc7465f257cdc8a46e9dbaa519f39
sungjh0726 <sungjh0726@naver.com>
2019-01-13 13:48:26 +0900

    연산자 대충

--- src/hijava/prac/N.java
+++ src/hijava/prac/N.java
@@ -21,8 +21,24 @@ public class N {
 		System.out.println("x=" + n.x);
 		System.out.println("y=" + n.y);
 		
-
-
+		int i = 10;
+		double d = 2.0;
+		
+		int r1 = (int)(i + d);
+		int r2 = (int)(i - d);
+		int r3 = (int)(i * d);
+		
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		
+		
+		double k = 12.4;
+		int p = 3;
+		int p4 = (int)(k / p)
+				
+		System.out.println();
+		
 	}
 
 }
